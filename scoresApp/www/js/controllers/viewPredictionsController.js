b5c7bbca9bcedd9f58fb3b3ebb25f0c1f238f72d
Jason Lynn <jason.lynn@advancedcomputersoftware.com>
2015-12-18 16:47:05 +0000

    Update view predictions screen.

--- scoresApp/www/js/controllers/viewPredictionsController.js
+++ scoresApp/www/js/controllers/viewPredictionsController.js
@@ -12,14 +12,30 @@ function viewPredictionsController(
   ) {
   dataService.checkUserAuthenticated();
 
+  $scope.matchWeeks = [];
+
+  $scope.init = function () {
+
+    var i;
+    for (i = 1; i <= 38; i++) {
+      $scope.matchWeeks.push(i);
+    }
+  }
+
   var userPredictions = new Firebase('https://ionic-scores.firebaseio.com/scores/user/'.concat(dataService.authenticatedUser.uid));
-  //var userPredictions = new Firebase('https://ionic-scores.firebaseio.com/scores/user/'.concat(authenticatedUser.uid).concat('/matchday/10'));
   userPredictions.on("value", function (snapshot) {
     $scope.predictions = snapshot.val();
   });
 
   $scope.matchdayChanged = function () {
-    var userPredictions = new Firebase('https://ionic-scores.firebaseio.com/scores/user/'.concat(dataService.authenticatedUser.uid).concat('/matchday/'.concat($scope.selectedMatchday)));
+    var userPredictions = new Firebase('https://ionic-scores.firebaseio.com/scores/user/'.concat($scope.selectedUser).concat('/matchday/'.concat($scope.selectedMatchday)));
+    userPredictions.on("value", function (snapshot) {
+      $scope.predictions = snapshot.val();
+    });
+  }
+
+  $scope.userChanged = function () {
+    var userPredictions = new Firebase('https://ionic-scores.firebaseio.com/scores/user/'.concat($scope.selectedUser).concat('/matchday/'.concat($scope.selectedMatchday)));
     userPredictions.on("value", function (snapshot) {
       $scope.predictions = snapshot.val();
     });
